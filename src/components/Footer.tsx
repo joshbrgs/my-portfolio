10d788de24757cc296b0eb3456a1cf61ba5ec70b
Josh Burgess <burgessj247@gmail.com>
2021-06-30 12:05:05 -0400

    resume btn fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,14 +20,7 @@ const Footer: React.FC = () => {
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
-        <img
-          src="/assets/download.svg"
-          alt="download"
-          className="dwn"
-          height={10}
-          width={35}
-        />{' '}
-        Resume
+        <img src="/assets/download.svg" alt="download" className="dwn" /> Resume
       </motion.a>
       <a href="mailto:burgessj247@gmail.com">burgessj247@gmail.com</a>
     </div>
